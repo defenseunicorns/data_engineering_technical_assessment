8aa410386e1e6600d5adf0a82f8299a0006d9239
Luke Griswold <ldgriswold@defenseunicorns.com>
2025-03-07 10:46:32 -0500

    chore: add data loaders for quickstart

--- src/ingest.py
+++ src/ingest.py
@@ -1,15 +1,32 @@
 import pandas as pd
+import json
 from sqlalchemy.sql import text
 from logs import setup_logging, get_logger
 from comms import test_connections, check_schema
 
 log = get_logger()
 
+DATA_PATH = "../data"
+
+def get_batch_data() -> pd.DataFrame:
+   """Returns a pandas dataframe for the batch orders parquet"""
+   return pd.read_parquet(f"{DATA_PATH}/batch_orders.parquet")
+
+def get_streaming_json() -> list:
+   """Returns a list of dictionaries of the streaming messages"""
+   with open(f"{DATA_PATH}/streaming_orders.json", "r") as fh:
+      messages = json.load(fh)
+   return messages
+
 # IMPLEMENT THIS METHOD
 def ingest_data():
-   pass
+   df = get_batch_data()
+   log.info(f'{df.head()}')
+   messages = get_streaming_json()
+   log.info(f'{messages[0]}')
 
 if __name__ == "__main__":
    setup_logging()
    test_connections()
    check_schema()
+   ingest_data()
